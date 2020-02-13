9d6bc4b1eef895f353ecff1d9c1535758f6cdaf9
Samar Panda <er.samarpanda@gmail.com>
2020-02-13 22:05:49 +0530

    Rendering site metadata using hooks & graphql static query

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: 'SamarPanda.com',
     description: 'Technical Architect at Quikr',
   },
-  plugins: ['gatsby-plugin-emotion'],
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
